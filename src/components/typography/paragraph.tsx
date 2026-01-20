1555ef6e9eab8fc14cffc041da289e37a67a81be
gmorikawa <morikawa.gabriel@outlook.jp>
2026-01-20 12:33:01 +0900

    refactor: Pass other properties down on Paragraph component

--- src/components/typography/paragraph.tsx
+++ src/components/typography/paragraph.tsx
@@ -1,18 +1,19 @@
-import { Text } from "@chakra-ui/react";
+import { Text, type TextProps } from "@chakra-ui/react";
 
 type Size = "sm" | "md" | "lg" | "xl" | "2xl" | "3xl" | "4xl" | "5xl" | "6xl";
 type Align = "left" | "center" | "right" | "justify";
 
-export interface ParagraphProps extends React.PropsWithChildren {
+export interface ParagraphProps extends TextProps {
     size?: Size;
     align?: Align;
 }
 
-export function Paragraph({ size, align, children }: ParagraphProps) {
+export function Paragraph({ size, align, children, ...props }: ParagraphProps) {
     return (
         <Text
             textStyle={size ?? "md"}
             textAlign={align ?? "left"}
+            {...props}
         >
             {children}
         </Text>
